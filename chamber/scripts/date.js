1d18affae2eea7bad941145afafc570f42662617
shaundradee <shaundra.d.strong@gmail.com>
2025-07-21 14:16:16 -0400

    commit to upload progress.

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -5,3 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const lastModified = document.lastModified;
     document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
 });
+
+//Hamburger button (adding it here so that it goes across all pages)
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+    const expanded = menuToggle.getAttribute('aria-expanded') === 'true';
+    menuToggle.setAttribute('aria-expanded', !expanded);
+});
+
---
+++ chamber/scripts/members.js
@@ -0,0 +1,55 @@
+const container = document.querySelector('#membersContainer');
+const gridBtn = document.querySelector('#gridView');
+const listBtn = document.querySelector('#listView');
+
+// Fetch and display the business members
+async function loadMembers() {
+    try {
+        const response = await fetch('data/members.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const members = await response.json();
+        displayMembers(members);
+    }
+    catch (error) {
+        console.error('Could not load members:', error);
+    }
+}
+
+function displayMembers(members) {
+    container.innerHTML = ''; // Clear existing content
+    members.forEach(member => {
+        const card = document.createElement('section');
+        card.classList.add('member-card');
+
+        card.innerHTML = `
+            <h3 class="member-name"><strong>${member.name}</strong></h3>
+            <p class="description">${member.description}</p>
+            <div class="card-content">
+                <img src="images/${member.image}" alt="${member.name} logo">
+                <div class="info">
+                    <p class="address"><strong>Address:</strong> ${member.address}</p>
+                    <p class="phone"><strong>Phone:</strong> ${member.phone}</p>
+                    <p class="website"><strong>Website:</strong> ${member.website}</p>
+                </div>
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+// Toggle Views
+gridBtn.addEventListener('click', () => {
+    container.classList.add('grid');
+    container.classList.remove('list');
+});
+
+listBtn.addEventListener('click', () => {
+    container.classList.add('list');
+    container.classList.remove('grid');
+});
+
+// Load on page load
+loadMembers();
